7fda50db251adc83f67904e81d02d2fbf3bfb728
charlesxiaolongcai <xiaolong.cai@tinklabs.com>
2018-12-21 10:58:48 +0800

    fix ecom1854 and ecom1858

--- src/pfComponent/datePicker.js
+++ src/pfComponent/datePicker.js
@@ -19,21 +19,32 @@ class Calendar extends React.Component {
         super(props);
         this.handleClick = this.handleClick.bind(this);
         this.state = {
-            currentIndex: 0
-        }
+            currentIndex: null,
+            preferedSodates: props.preferedDates
+        };
     }
     
-    handleClick(event) {
+    //切换package
+    componentWillReceiveProps(nextProps){
         this.setState({
-            currentIndex: parseInt(event.currentTarget.getAttribute('index')),
-            // selectedDate: event.currentTarget.getAttribute('value')
-        });
-        this.props.onHandleDate(event.currentTarget.getAttribute('value'));
-        
-       //再次点击date 显示隐藏quantity下的模块
-       if((this.props.belowFlagOne && this.props.belowFlagTwo) || this.props.belowFlagOne || this.props.belowFlagTwo){
-        this.props.onHandleDownFlag(false,false,'buttonText');
+            preferedSodates: nextProps.preferedDates
+        })
+    }
+    handleClick(event) {
+        if(event.currentTarget.getAttribute('prefereddatevalue') === '1'){
+            this.setState({
+                currentIndex: parseInt(event.currentTarget.getAttribute('index')),
+            },function(){
+                this.state.currentIndex = null;
+            });
+            this.props.onHandleDate(event.currentTarget.getAttribute('value'));
+            
+           //再次点击date 显示隐藏quantity下的模块
+           if((this.props.belowFlagOne && this.props.belowFlagTwo) || this.props.belowFlagOne || this.props.belowFlagTwo){
+            this.props.onHandleDownFlag(false,false,'buttonText');
+            }
         }
+        
     }
     
     render() {
@@ -41,12 +52,20 @@ class Calendar extends React.Component {
         const week = ["Sun", "Mon", "Tue", "Wed", "Tur", "Fri", "Sat"];
         const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
         const now = new Date();
+        let preferedDateFlag = [];
+        let prefereddateFlag = [];
         const items = numbers.map((i) => {
             const date = new Date(+now + i * 3600 * 1000 * 24).getDate();
             const day = new Date(+now + i * 3600 * 1000 * 24).getDay();
+            for(let j = 0; j < this.state.preferedSodates.length; j++){
+                if(+date === Number(this.state.preferedSodates[j])){
+                    preferedDateFlag[i] = true;
+                    prefereddateFlag[i] = 1;
+                }
+            }
             return (<li id="item">
                 <p>{week[day]}</p>
-                <p onClick={this.handleClick} key={i} value={date < 10? '0'+date:date} index={i} className={this.state.currentIndex === i ? 'current' : ''}>{date < 10? '0'+date:date}</p>
+                <p onClick={this.handleClick} prefereddatevalue={prefereddateFlag[i]} style={{color : (preferedDateFlag[i] ? '#000f23':'#e6e6e6')}} value={date < 10? '0'+date:date} index={i} className={this.state.currentIndex === i ? 'current' : ''}>{date < 10? '0'+date:date}</p>
             </li>)
         });
         return (
@@ -102,14 +121,14 @@ class DatePicker extends React.Component {
         this.props.onHandleBelowFlag(flagone,flagtwo,index);
     }
     render() {
-        console.log(this.state.selectDate);
         let pickDate = this.state.selectDate;
         let timeslotTittle = [];
+        let preferedDate = [];
         if(this.props.packages != undefined){
-            // console.log(this.props.packages.dates);
+            console.log(this.props.packages.dates);
             for(let i = 0; i<this.props.packages.dates.length; i++){
-                if(pickDate == this.props.packages.dates[i].date.substring(8,10)){
-                    // console.log(this.props.packages.dates[i].timeslots);
+                preferedDate[i] = this.props.packages.dates[i].date.substring(8,10); //没有传来的date禁止
+                if(pickDate == this.props.packages.dates[i].date.substring(8,10)){         
                     timeslotTittle = this.props.packages.dates[i].timeslots;                   
                 }
             }
@@ -131,7 +150,7 @@ class DatePicker extends React.Component {
                     <div className='datePickerHead'>{Month[month]} {year}</div>
                     <div className="calendar">
                         <ul className="list">
-                            <Calendar  onHandleDate={this.handleDate} onHandleDownFlag={this.handleDownFlag} belowFlagOne={this.state.nextstate} belowFlagTwo={this.state.afterFirstConfirm}/>
+                            <Calendar  onHandleDate={this.handleDate} onHandleDownFlag={this.handleDownFlag} belowFlagOne={this.state.nextstate} belowFlagTwo={this.state.afterFirstConfirm} preferedDates={preferedDate}/>
                         </ul>
                     </div>
                 </div>
